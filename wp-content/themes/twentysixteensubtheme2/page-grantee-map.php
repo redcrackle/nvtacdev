b70304eb7aa38d3e0259c52cb918e072156262e0
Red Crackle <sathish2006cbe@gmail.com>
2022-06-03 17:20:37 +0530

    fixed grantee map template

--- wp-content/themes/twentysixteensubtheme2/page-grantee-map.php
+++ wp-content/themes/twentysixteensubtheme2/page-grantee-map.php
@@ -12,8 +12,33 @@ wp_head();
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<div id="grantee-map"></div>
-	</main><!-- .site-main -->
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <header class="entry-header">
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+      </header><!-- .entry-header -->
+
+      <?php twentysixteen_post_thumbnail(); ?>
+
+      <div class="entry-content">
+
+      <div id="grantee-map"></div>
+      </div><!-- .entry-content -->
+
+      <?php
+      edit_post_link(
+        sprintf(
+        /* translators: %s: Post title. */
+          __( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+          get_the_title()
+        ),
+        '<footer class="entry-footer"><span class="edit-link">',
+        '</span></footer><!-- .entry-footer -->'
+      );
+      ?>
+
+    </article><!-- #post-<?php the_ID(); ?> -->
+
+  </main><!-- .site-main -->
 
 	<?php get_sidebar( 'content-bottom' ); ?>
 
