ea1a78a24ee705cf5ab5b6d13512156f44a18c34
Red Crackle <sathish2006cbe@gmail.com>
2022-06-09 21:32:59 +0530

    modified grantee map

--- wp-content/themes/twentysixteensubtheme2/map/build/index.asset.php
+++ wp-content/themes/twentysixteensubtheme2/map/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '1e6b06c1b59621b0da86');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'e843f111f22f4f928c45');
