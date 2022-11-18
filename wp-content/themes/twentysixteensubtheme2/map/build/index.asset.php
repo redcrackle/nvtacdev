26d85fe70062476939a02eaace30362765bfc999
Shan <shantanu@zantrik.in>
2022-11-18 14:06:35 +0530

    Changes to phone number in map file

--- wp-content/themes/twentysixteensubtheme2/map/build/index.asset.php
+++ wp-content/themes/twentysixteensubtheme2/map/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '02372145229336714e9c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'ddb4c40a01c23348846c');
