7582abe1d85dff7e64be2454cb487dc402378a42
sunil <sunil.kumar@redcrackle.com>
2025-07-07 17:56:06 +0530

    code updated

---
+++ wp-content/themes/twentysixteensubtheme2/page-grantee-dashboard.php
@@ -0,0 +1,29 @@
+<?php
+ /*
+   Template Name: Grantee Map Dashboard    
+ */
+
+
+get_header();
+
+wp_head();
+?>
+
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <header class="entry-header">
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+      </header><!-- .entry-header -->
+
+      <?php twentysixteen_post_thumbnail(); ?>
+
+      <div>
+        <?php
+		the_content();
+		?>
+
+    </article><!-- #post-<?php the_ID(); ?> -->
+
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
